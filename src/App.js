f2c67521851426074f0842c375d07325bc5491ff
Yeoony <83230013+jeongyummy@users.noreply.github.com>
2021-12-30 23:44:27 +0900

    [feat] ReducerUser - 리듀서 이용한 api 호출

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import Users from "./components/Users";
+import ReducerUser from "./components/ReducerUser";
 
 function App() {
   return (
     <div className="App">
       <Users />
+      <br />
+      <ReducerUser />
     </div>
   );
 }
---
+++ src/components/ReducerUser.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useReducer } from "react";
+import axios from "axios";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "LOADING":
+      return {
+        loading: true,
+        data: null,
+        error: null,
+      };
+    case "SUCCESS":
+      return {
+        loading: false,
+        data: action.data,
+        error: null,
+      };
+    case "ERROR":
+      return {
+        loading: false,
+        data: null,
+        error: action.error,
+      };
+    default:
+      throw new Error(`다루지 액션 타입입니다: ${action.type}`);
+  }
+}
+function Users() {
+  const [state, dispatch] = useReducer(reducer, {
+    loading: false,
+    data: null,
+    error: null,
+  });
+
+  const fetchUsers = async () => {
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      dispatch({ type: "SUCCESS", data: response.data });
+    } catch (e) {
+      dispatch({ type: "ERROR", data: e });
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const { loading, data: users, error } = state;
+
+  if (loading) return <div> 로딩중... </div>;
+  if (error) return <div> 에러가 발생했습니다.</div>;
+  if (!users) return null;
+
+  return (
+    <>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.username} ({user.name})
+          </li>
+        ))}
+      </ul>
+      <button onClick={fetchUsers}> 다시 불러오기 </button>
+    </>
+  );
+}
+
+export default Users;
