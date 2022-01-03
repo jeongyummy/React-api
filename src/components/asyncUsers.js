cbbf06082bb622ee25ca423e58e8a793d5e132f2
Yeoony <83230013+jeongyummy@users.noreply.github.com>
2022-01-03 19:11:49 +0900

    [feat] asycUsers - 비동기 호출

---
+++ src/components/asyncUsers.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import axios from 'axios';
+import useAsync from './useAsync';
+
+async function getUsers() {
+  const response = await axios.get(
+    'https://jsonplaceholder.typicode.com/users'
+  );
+  return response.data;
+}
+
+
+function User({ id }) {
+  const [state] = useAsync(() => getUsers(id), [id]);
+  const { loading, data: user, error } = state;
+
+  if(loading) return <div>로딩중</div>;
+  if(error) return <div>에러가 발생했습니다</div>
+  if(!user) return null;
+
+  return (
+    <div>
+        <h2>{user.username}</h2>
+        <p>
+          <b>Email:</b> {user.email}
+        </p>
+    </div>
+  );
+}
+
+export default User;
+
