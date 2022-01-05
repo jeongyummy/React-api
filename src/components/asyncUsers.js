9b7a35e4ddf2e2a2ec7ffa6fbe09af3c248aa10d
Yeoony <83230013+jeongyummy@users.noreply.github.com>
2022-01-05 18:29:01 +0900

    [fix] asycUsers - 비동기 부분 return 아래값 추가

--- src/components/asyncUsers.js
+++ src/components/asyncUsers.js
@@ -1,32 +1,34 @@
-import React from 'react';
-import axios from 'axios';
-import useAsync from './useAsync';
+import React from "react";
+import axios from "axios";
+import useAsync from "./useAsync";
 
 async function getUsers() {
   const response = await axios.get(
-    'https://jsonplaceholder.typicode.com/users'
+    "https://jsonplaceholder.typicode.com/users"
   );
   return response.data;
 }
 
-
-function User({ id }) {
-  const [state] = useAsync(() => getUsers(id), [id]);
+function Users() {
+  const [state, refetch] = useAsync(getUsers, []);
   const { loading, data: user, error } = state;
 
-  if(loading) return <div>로딩중</div>;
-  if(error) return <div>에러가 발생했습니다</div>
-  if(!user) return null;
+  if (loading) return <div>로딩중...</div>;
+  if (error) return <div>에러가 발생했습니다</div>;
+  if (!user) return null;
 
   return (
-    <div>
-        <h2>{user.username}</h2>
-        <p>
-          <b>Email:</b> {user.email}
-        </p>
-    </div>
+    <>
+      <ul>
+        {Users.map((user) => (
+          <li key={user.id}>
+            {user.username} ({user.name})
+          </li>
+        ))}
+      </ul>
+      <button onClick={refetch}>다시 불러오기</button>
+    </>
   );
 }
 
-export default User;
-
+export default Users;
